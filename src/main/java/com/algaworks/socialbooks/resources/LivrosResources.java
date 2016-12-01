879f3bca51a039094a14e0c3bf2cb62c415b0cf0
Thiago Santos <thiago.santos@m4u.com.br>
2016-12-01 14:49:10 -0200

    Primeiro commit - Aula 2.6 - Listando e adiconando livros

---
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,31 @@
+package com.algaworks.socialbooks.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.repository.LivrosRepository;
+
+@RestController
+@RequestMapping("/livros")
+public class LivrosResources {
+	
+	@Autowired 
+	private LivrosRepository livrosRepository;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Livro> listar(){
+		return livrosRepository.findAll();		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro){
+		livrosRepository.save(livro);
+	}
+
+}
