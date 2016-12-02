41994b2f380576be376d28065cb53fba9fa43f1c
Thiago Santos <thiago.santos@m4u.com.br>
2016-12-02 10:49:51 -0200

    Métodos buscar,deletar e put - Aula 2.9

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +26,22 @@ public class LivrosResources {
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public void salvar(@RequestBody Livro livro){
+		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public Livro buscar(@PathVariable("id") Long id){
+		return livrosRepository.findOne(id);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id") Long id){
+		livrosRepository.delete(id);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id){
+		livro.setId(id);
 		livrosRepository.save(livro);
 	}
 
